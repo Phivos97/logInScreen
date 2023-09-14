a50905eca0999db74dd06583ce3f2bd43ca7033c
Crispon <50478018+Crispon@users.noreply.github.com>
2023-09-14 11:56:21 +0300

    - Code Review & Refactor

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,131 +1,116 @@
-import { React, useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const SignIn = () => {
-  const [logInUser, setLogInUser] = useState('');
-  const [logInPassword, setLogInPassword] = useState('');
-  const [passwordShown, setPasswordShown] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [toggleError, setToggleError] = useState(false);
+	const [logInUser, setLogInUser] = useState('');
+	const [logInPassword, setLogInPassword] = useState('');
+	const [passwordShown, setPasswordShown] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+	const [toggleError, setToggleError] = useState(false);
 
-  const togglePassword = () => {
-    setPasswordShown(!passwordShown);
-  };
-  const handleChangeLogInUser = (event) => {
-    setLogInUser(event.target.value);
-  };
-  const handleChangeLogInPassword = (event) => {
-    setLogInPassword(event.target.value);
-  };
+	const checkLogInUser = name => {
+		return name.username === `${logInUser}` && name.password === `${logInPassword}`;
+	};
 
-  const checkLogInUser = (name) => {
-    return (
-      name.username === `${logInUser}` && name.password === `${logInPassword}`
-    );
-  };
+	const handleLogIn = () => {
+		if (JSON.parse(localStorage.getItem('users')).find(checkLogInUser)) return console.log(`Logged in as ${logInUser}`);
 
-  const handleLogIn = () => {
-    if (JSON.parse(localStorage.getItem('users')).find(checkLogInUser)) {
-      return console.log(`Logged in as ${logInUser}`);
-    } else setToggleError(true);
-    return setErrorMessage('Incorrect username or password');
-    console.log('incorrect username or password');
-  };
-  return (
-    <>
-      <div className='relative h-screen flex justify-center items-center text-white'>
-        <div
-          style={{ backgroundImage: "url('/mountains.jpg')" }}
-          className='inset-0 bg-center bg-cover absolute brightness-95'
-        ></div>
-        <form
-          className='relative flex flex-col bg-transparent max-w-[400px] w-full '
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <span className='text-4xl pb-6 text-center mb-2'>
-            Have an account?
-          </span>
+		setToggleError(true);
+		return setErrorMessage('Incorrect username or password');
+	};
+	return (
+		<>
+			<div className='relative h-screen flex justify-center items-center text-white'>
+				<div style={{ backgroundImage: "url('/mountains.jpg')" }} className='inset-0 bg-center bg-cover absolute brightness-95'></div>
 
-          <input
-            className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
-            placeholder='Username'
-            size='25'
-            type='text'
-            onChange={handleChangeLogInUser}
-            value={logInUser}
-          />
-          <i
-            onClick={togglePassword}
-            className={
-              passwordShown
-                ? 'fa fa-eye-slash absolute top-[162px] right-[21px] hover:cursor-pointer z-50'
-                : 'fa fa-eye absolute top-[162px] right-[21px] hover:cursor-pointer z-50'
-            }
-          ></i>
-          <input
-            className=' backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl bg-slate-50/20 placeholder-white p-5 transition focus:ease-in focus:duration-[300ms] focus:outline-none focus:bg-slate-50/25 shadow-lg'
-            placeholder='Password'
-            size='25'
-            type={passwordShown ? 'text' : 'password'}
-            onChange={handleChangeLogInPassword}
-            value={logInPassword}
-          />
-          <Link
-            // to={
-            //   JSON.parse(localStorage.getItem('users')).find(checkLogInUser)
-            //     ? '/welcome'
-            //     : '/'
-            // }
-            className='backdrop-blur-xl bg-[hsla(4, 67%, 82%, 1)] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)] mb-2 text-xl font-semibold  opacity-95 py-3 px-3 rounded-3xl text-black hover:brightness-[1.07] hover:cursor-pointer hover:opacity-95 text-center transition-all duration-[500ms] ease-out'
-            onClick={handleLogIn}
-          >
-            SIGN IN
-          </Link>
+				<form className='relative flex flex-col max-w-[400px] w-full' onSubmit={e => e.preventDefault()}>
+					<span className='text-4xl pb-6 text-center mb-2'>Have an account?</span>
 
-          <div className='flex justify-between mb-6'>
-            <label className='flex'>
-              <input
-                type='checkbox'
-                className='peer w-4 h-4 accent-[#F0B7B3]  rounded-xl hover:ring-red-600 ring-0 self-center point hover:accent-[#F0B7B3] hover:cursor-pointer checked:hover:'
-                // checked='checked'
-              />
+					{/* Username Input */}
+					<input
+						className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
+						placeholder='Username'
+						size='25'
+						type='text'
+						onChange={event => {
+							setLogInUser(event.target.value);
+						}}
+						value={logInUser}
+					/>
 
-              <span className='peer-checked:text-[#F0B7B3] text-xl py-3 px-3 text-white hover:text-[#F0B7B3] hover:cursor-pointer brightness-110 '>
-                Remember Me
-              </span>
-              {/* Remember me */}
-            </label>
-            <label>
-              <input
-                className='text-xl py-3 px-3 rounded-3xl justify-center text-white hover:text-[#F0B7B3] hover:cursor-pointer brightness-110 '
-                type='submit'
-                value='Forgot Password'
+					{/* Eye Icon */}
+					<i
+						onClick={() => {
+							setPasswordShown(!passwordShown);
+						}}
+						className={
+							passwordShown ? 'fa fa-eye-slash absolute top-[162px] right-[21px] hover:cursor-pointer z-50' : 'fa fa-eye absolute top-[162px] right-[21px] hover:cursor-pointer z-50'
+						}></i>
 
-                // onClick={navigate('/signup')}
-                // onClick={<Link to='/signup'></Link>}
-              />
-            </label>
-          </div>
-          <span className='text-center  text-xl mb-5'>— Or Sign In With —</span>
+					{/* Password Input */}
+					<input
+						className=' backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl bg-slate-50/20 placeholder-white p-5 transition focus:ease-in focus:duration-[300ms] focus:outline-none focus:bg-slate-50/25 shadow-lg'
+						placeholder='Password'
+						size='25'
+						type={passwordShown ? 'text' : 'password'}
+						// onChange={(event)=>{setLogInPassword(event.target.value);}}
+						onChange={event => {
+							setLogInPassword(event.target.value);
+						}}
+						value={logInPassword}
+					/>
+					<Link
+						// to={
+						//   JSON.parse(localStorage.getItem('users')).find(checkLogInUser)
+						//     ? '/welcome'
+						//     : '/'
+						// }
+						className='backdrop-blur-xl bg-[hsla(4, 67%, 82%, 1)] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)] mb-2 text-xl font-semibold  opacity-95 py-3 px-3 rounded-3xl text-black hover:brightness-[1.07] hover:cursor-pointer hover:opacity-95 text-center transition-all duration-[500ms] ease-out'
+						onClick={handleLogIn}>
+						SIGN IN
+					</Link>
 
-          <div className='flex justify-between gap-3'>
-            <a
-              className='backdrop-blur-3xl hover:opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black hover:bg-[#F0B7B3] hover:brightness-110 hover:cursor-pointer text-center transition ease-in-out duration-[600ms] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)]'
-              href='https://www.facebook.com'
-            >
-              Facebook
-            </a>
+					{/* Rest of Buttons */}
+					<div className='flex justify-between mb-6'>
+						{/* Remember Me */}
+						<div className='flex'>
+							<input
+								type='checkbox'
+								className='peer w-4 h-4 accent-myOrange  rounded-xl hover:ring-red-600 ring-0 self-center point hover:accent-myOrange hover:cursor-pointer checked:hover:'
+								// checked='checked'
+							/>
 
-            {/* // backdrop-blur-3xl brightness-150 opacity-80 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black hover:bg-[#F0B7B3] hover:brightness-110 hover:cursor-pointer hover:opacity-95 text-center transition ease-in-out duration-[600ms] bg-gradient-to-r from-[hsla(4,67%,82%,1)] to-[hsla(47,100%,87%,1)] */}
-            <a
-              className='backdrop-blur-3xl hover:opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black hover:bg-[#F0B7B3] hover:brightness-110 hover:cursor-pointer text-center transition ease-in-out duration-[600ms] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)]'
-              href='https://www.twitter.com'
-            >
-              Twitter
-            </a>
-          </div>
+							<span className='peer-checked:text-myOrange text-xl py-3 px-3 text-white hover:text-myOrange hover:cursor-pointer brightness-110 '>Remember Me</span>
+							{/* Remember me */}
+						</div>
 
-          {/* <label>
+						<input
+							className='text-xl py-3 px-3 rounded-3xl justify-center text-white hover:text-myOrange hover:cursor-pointer brightness-110 '
+							type='submit'
+							value='Forgot Password'
+
+							// onClick={navigate('/signup')}
+							// onClick={<Link to='/signup'></Link>}
+						/>
+					</div>
+					<span className='text-center  text-xl mb-5'>— Or Sign In With —</span>
+
+					<div className='flex gap-3'>
+						<a
+							className='backdrop-blur-3xl hover:opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black hover:bg-myOrange hover:brightness-110 hover:cursor-pointer text-center transition ease-in-out duration-[600ms] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)]'
+							href='https://www.facebook.com'>
+							Facebook
+						</a>
+
+						{/* // backdrop-blur-3xl brightness-150 opacity-80 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black hover:bg-myOrange hover:brightness-110 hover:cursor-pointer hover:opacity-95 text-center transition ease-in-out duration-[600ms] bg-gradient-to-r from-[hsla(4,67%,82%,1)] to-[hsla(47,100%,87%,1)] */}
+						<a
+							className='backdrop-blur-3xl hover:opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black hover:bg-myOrange hover:brightness-110 hover:cursor-pointer text-center transition ease-in-out duration-[600ms] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)]'
+							href='https://www.twitter.com'>
+							Twitter
+						</a>
+					</div>
+
+					{/* <label>
             <div className='flex justify-between'>
               <input
                 className='text-xl font-bold border-solid bg-red-50 w-24 py-3 px-3 rounded-3xl justify-center text-black'
@@ -144,17 +129,18 @@ export const SignIn = () => {
               </Link>
             </div>
           </label> */}
-        </form>
-        {toggleError ? (
-          <span className='absolute bottom-0 backdrop-blur-3xl opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black text-center transition ease-in-out duration-[600ms] bg-red-500'>
-            {errorMessage}
-          </span>
-        ) : null}
-      </div>
+				</form>
 
-      {/* </div> */}
-    </>
-  );
+				{toggleError ? (
+					<span className='absolute bottom-0 backdrop-blur-3xl opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black text-center transition ease-in-out duration-[600ms] bg-red-500'>
+						{errorMessage}
+					</span>
+				) : null}
+			</div>
+
+			{/* </div> */}
+		</>
+	);
 };
 
-// w-full font-semibold text-xl border-solid bg-slate-50/90 py-3 px-3 rounded-lg justify-center text-black hover:bg-[#F0B7B3] hover:brightness-110 hover:cursor-pointer hover:opacity-95 text-center transition ease-in duration-[2000ms] hover:bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)]
+// w-full font-semibold text-xl border-solid bg-slate-50/90 py-3 px-3 rounded-lg justify-center text-black hover:bg-myOrange hover:brightness-110 hover:cursor-pointer hover:opacity-95 text-center transition ease-in duration-[2000ms] hover:bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)]
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,155 +1,142 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export const SignUp = () => {
-  const [user, setUser] = useState('');
-  const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [toggleError, setToggleError] = useState(false);
-  const [toggleSuccess, setToggleSuccess] = useState(false);
-  const [users, setUsers] = useState(() => {
-    if (JSON.parse(localStorage.getItem('users'))) {
-      return JSON.parse(localStorage.getItem('users'));
-    } else {
-      return [];
-    }
-  });
+	const [user, setUser] = useState('');
+	const [password, setPassword] = useState('');
+	const [password2, setPassword2] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+	const [toggleError, setToggleError] = useState(false);
+	const [toggleSuccess, setToggleSuccess] = useState(false);
+	const [users, setUsers] = useState(() => {
+		if (JSON.parse(localStorage.getItem('users'))) {
+			return JSON.parse(localStorage.getItem('users'));
+		} else {
+			return [];
+		}
+	});
 
-  const handleChangeUser = (event) => {
-    setUser(event.target.value);
-  };
+	const setBackground = () => {
+		if (toggleError) return 'bg-red-500';
+		if (toggleSuccess) return 'bg-green-500';
+		return 'invisible';
+	};
 
-  const handleChangePassword = (event) => {
-    setPassword(event.target.value);
-  };
-  const handleChangePassword2 = (event) => {
-    setPassword2(event.target.value);
-  };
+	const handleChangeUser = event => {
+		setUser(event.target.value);
+	};
 
-  const isName = (name) => {
-    return name.username === `${user}`;
-  };
+	const handleChangePassword = event => {
+		setPassword(event.target.value);
+	};
+	const handleChangePassword2 = event => {
+		setPassword2(event.target.value);
+	};
 
-  const clearFields = () => {
-    setUser('');
-    setPassword('');
-    setPassword2('');
-  };
+	const clearFields = () => {
+		setUser('');
+		setPassword('');
+		setPassword2('');
+	};
 
-  const handleSignUp = () => {
-    if (user.length < 5 || password.length < 5) {
-      setToggleSuccess(false);
-      setToggleError(true);
-      return setErrorMessage(
-        'Username and password fields must contain at least 5 characters'
-      );
-    }
-    if (JSON.parse(localStorage.getItem('users')).find(isName)) {
-      setToggleSuccess(false);
+	const handleSignUp = () => {
+		let success = 1;
+		let errorMessage = '';
 
-      setToggleError(true);
-      return setErrorMessage('User already exists');
-    }
-    if (password != password2) {
-      setToggleSuccess(false);
+		if (user.length < 5 || password.length < 5) {
+			success = 0;
+			errorMessage = 'Username and password fields must contain at least 5 characters';
+		}
+		if (JSON.parse(localStorage.getItem('users')).find(item => item.username === `${user}`)) {
+			success = 0;
+			errorMessage = 'User already exists';
+		}
+		['Vaggelis', 'Nikos', 'Kostas'];
+		if (password != password2) {
+			success = 0;
+			errorMessage = "Passwords don't match";
+		}
 
-      setToggleError(true);
-      return setErrorMessage("Passwords don't match");
-    } else {
-      setToggleError(false);
-      setToggleSuccess(true);
-      setErrorMessage('Successful sign up');
-      return setUsers([...users, { username: user, password: password }]);
-    }
-  };
+		if (success != 1) {
+			setToggleSuccess(false);
+			setToggleError(true);
+			setErrorMessage(errorMessage);
+		} else {
+			setToggleError(false);
+			setToggleSuccess(true);
+			setErrorMessage('Successful sign up');
+			return setUsers([...users, { username: user, password: password }]);
+		}
+	};
 
-  useEffect(() => {
-    localStorage.setItem('users', JSON.stringify(users));
-  }, [users]);
+	useEffect(() => {
+		localStorage.setItem('users', JSON.stringify(users));
+	}, [users]);
 
-  return (
-    <>
-      <div className='relative h-screen flex justify-center items-center text-white'>
-        <div
-          style={{ backgroundImage: "url('/mountains.jpg')" }}
-          className='inset-0 bg-center bg-cover absolute brightness-95'
-        ></div>
-        SIGN UP
-        <form
-          className='relative flex flex-col bg-transparent max-w-[400px] w-full'
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <span className='text-4xl pb-6 text-center mb-2'>
-            Create an account
-          </span>
+	return (
+		<>
+			<div className='relative h-screen flex justify-center items-center text-white'>
+				<div style={{ backgroundImage: "url('/mountains.jpg')" }} className='inset-0 bg-center bg-cover absolute brightness-95'></div>
+				SIGN UP
+				<form className='relative flex flex-col bg-transparent max-w-[400px] w-full' onSubmit={e => e.preventDefault()}>
+					<span className='text-4xl pb-6 text-center mb-2'>Create an account</span>
 
-          <input
-            className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
-            size='25'
-            placeholder='Username'
-            type='text'
-            onChange={handleChangeUser}
-            value={user}
-            required
-          />
+					<input
+						className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
+						size='25'
+						placeholder='Username'
+						type='text'
+						onChange={handleChangeUser}
+						value={user}
+						required
+					/>
 
-          <input
-            className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
-            size='25'
-            placeholder='Password'
-            type='password'
-            onChange={handleChangePassword}
-            value={password}
-            required
-          />
+					<input
+						className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
+						size='25'
+						placeholder='Password'
+						type='password'
+						onChange={handleChangePassword}
+						value={password}
+						required
+					/>
 
-          <input
-            className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
-            placeholder='Confirm password'
-            size='25'
-            type='password'
-            onChange={handleChangePassword2}
-            value={password2}
-            required
-          />
+					<input
+						className='backdrop-blur-lg text-xl h-12 mb-6 rounded-3xl  bg-slate-50/20 placeholder-white p-5 transition ease-in duration-[200ms] focus:outline-none  focus:bg-slate-50/25 shadow-lg'
+						placeholder='Confirm password'
+						size='25'
+						type='password'
+						onChange={handleChangePassword2}
+						value={password2}
+						required
+					/>
 
-          {/* <Link to='/'> */}
-          <input
-            className='backdrop-blur-xl bg-[hsla(4, 67%, 82%, 1)] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)] mb-6 text-xl font-semibold  opacity-95 py-3 px-3 rounded-3xl text-black hover:brightness-[1.07] hover:cursor-pointer hover:opacity-95 text-center transition-all duration-[500ms] ease-out'
-            type='submit'
-            value='CREATE ACCOUNT'
-            onClick={() => {
-              handleSignUp();
-              clearFields();
-            }}
-          />
-          <Link
-            className='text-center hover:text-[#F0B7B3] hover:cursor-pointer brightness-110'
-            to='/'
-          >
-            <span className=' text-xl mb-5'>— Have An Account? —</span>
-          </Link>
-        </form>
-        <span
-          className={`absolute bottom-0 backdrop-blur-3xl opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black text-center transition ease-in-out duration-[600ms] ${
-            toggleError
-              ? 'bg-red-500'
-              : toggleSuccess
-              ? 'bg-green-500'
-              : 'invisible'
-          }
-            } `}
-        >
-          {errorMessage}
-        </span>
-      </div>
-    </>
-  );
+					{/* <Link to='/'> */}
+					<input
+						className='backdrop-blur-xl bg-[hsla(4, 67%, 82%, 1)] bg-gradient-to-r from-[hsla(4,67%,82%,0.7)] to-[hsla(47,100%,87%,0.7)] mb-6 text-xl font-semibold  opacity-95 py-3 px-3 rounded-3xl text-black hover:brightness-[1.07] hover:cursor-pointer hover:opacity-95 text-center transition-all duration-[500ms] ease-out'
+						type='submit'
+						value='CREATE ACCOUNT'
+						onClick={() => {
+							handleSignUp();
+							clearFields();
+						}}
+					/>
+					<Link className='text-center hover:text-myOrange hover:cursor-pointer brightness-110' to='/'>
+						<span className=' text-xl mb-5'>— Have An Account? —</span>
+					</Link>
+				</form>
+				<span
+					className={`absolute bottom-0 backdrop-blur-3xl opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black text-center transition ease-in-out duration-[600ms] ${setBackground()}
+            } `}>
+					{errorMessage}
+				</span>
+			</div>
+		</>
+	);
 };
 
 {
-  /* <span
+	/* <span
           className={`${toggleSuccess? 'bg-green-500':}absolute bottom-0 backdrop-blur-3xl opacity-95 brightness-150 w-full font-semibold text-xl border-solid  py-3 px-3 rounded-lg justify-center text-black text-center transition ease-in-out duration-[600ms] bg-red-500`}
         >
           {errorMessage}
